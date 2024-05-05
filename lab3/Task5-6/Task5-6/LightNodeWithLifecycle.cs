5d206f5d907639acc990c33ff27eeec3c1897908
Yegor Zarembytskyi <ipz222_zyeyu@student.ztu.edu.ua>
2024-05-05 12:53:03 +0300

    Added a Template Method feature

---
+++ lab3/Task5-6/Task5-6/LightNodeWithLifecycle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_6
+{
+    public abstract class LightNodeWithLifecycle: LightNode
+    {
+        public virtual void OnCreated() { }
+        public virtual void OnInserted() { }
+        public virtual void OnRemoved() { }
+        public virtual void OnStylesApplied() { }
+        public virtual void OnClassListApplied() { }
+        public virtual void OnTextRendered() { }
+    }
+}
--- lab3/Task5-6/Task5-6/Program.cs
+++ lab3/Task5-6/Task5-6/Program.cs
@@ -2,48 +2,15 @@
 using System.Text;
 using Task5_6;
 
-string[] lines = File.ReadAllLines("D:\\КПЗ\\KPZ\\lab3\\Task5-6\\Task5-6\\book.txt");
+var cssClasses = new List<string> { "main", "highlight" };
+var root = new LightElementNodeWithLifecycle("div", "block", "normal", cssClasses);
+var innerDiv = new LightElementNodeWithLifecycle("div", "inline-block", "normal", new List<string> { "inner-div" });
 
-var html = new LightElementNode("html", "block", "closing", new List<string>());
-var h1 = new LightElementNode("h1", "block", "closing", new List<string>());
-h1.Children.Add(LightTextNode.GetTextNode(lines[0]));
-html.Children.Add(h1);
-
-for (int i = 1; i < lines.Length; i++)
-{
-    string line = lines[i];
-
-    if (line.Length < 20)
-    {
-        var h2 = new LightElementNode("h2", "block", "closing", new List<string>());
-        h2.Children.Add(LightTextNode.GetTextNode(lines[i]));
-        html.Children.Add(h2);
-    }
-    else if (!string.IsNullOrEmpty(line) && char.IsWhiteSpace(line[0]))
-    {
-        var blockquote = new LightElementNode("blockquote", "block", "closing", new List<string>());
-        blockquote.Children.Add(LightTextNode.GetTextNode(lines[i]));
-        html.Children.Add(blockquote);
-    }
-    else
-    {
-        var p = new LightElementNode("p", "block", "closing", new List<string>());
-        p.Children.Add(LightTextNode.GetTextNode(lines[i]));
-        html.Children.Add(p);
-    }
-}
-
-html.PrintOuterHTML();
-
-long totalMemory = CalculateMemoryUsage(html);
-Console.WriteLine($"Size of tree: {totalMemory} byte");
-static long CalculateMemoryUsage(object obj)
-{
-    GC.Collect();
-    GC.WaitForPendingFinalizers();
-    GC.Collect();
-
-    long totalMemory = GC.GetTotalMemory(true);
-
-    return totalMemory;
-}
+root.InsertChild(innerDiv);
+root.ApplyStylesAndClasses();
+root.PrintOuterHTML();
+var textNode = LightTextNode.GetTextNode("Hello, World!");
+innerDiv.InsertChild(textNode);
+root.PrintOuterHTML();
+root.RemoveChild(innerDiv);
+root.PrintOuterHTML();
