2854a3dfe7fee466d341f11a2d60a45a01a30e8c
Yegor Zarembytskyi <ipz222_zyeyu@student.ztu.edu.ua>
2024-05-05 13:45:59 +0300

    Added a State feature

---
+++ lab3/Task5-6/Task5-6/DisabledState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_6
+{
+    public class DisabledState : IElementState
+    {
+        public void Apply(LightElementNodeWithLifecycle element)
+        {
+            if (!element.CssClasses.Contains("disabled"))
+            {
+                element.CssClasses.Add("disabled");
+            }
+            Console.WriteLine($"Element <{element.TagName}> is now disabled.");
+        }
+    }
+}
---
+++ lab3/Task5-6/Task5-6/VisibleState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_6
+{
+    public class VisibleState : IElementState
+    {
+        public void Apply(LightElementNodeWithLifecycle element)
+        {
+            element.CssClasses.Remove("hidden");
+            Console.WriteLine($"Element <{element.TagName}> is now visible.");
+        }
+    }
+}
